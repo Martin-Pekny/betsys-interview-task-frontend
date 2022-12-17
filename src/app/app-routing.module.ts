914057820cdc3e05448d956e1cc8b1d40245dfcd
Martin Pekný <84441995+Maperti@users.noreply.github.com>
2022-12-17 21:54:04 +0100

    Revert "feature: add base project module structure / simple nodejs server  / angular material module"
    This reverts commit c079310dcbbad1d310ee1a4da923e357f41b9f3f.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,17 +2,10 @@ import {NgModule} from '@angular/core';
 // eslint-disable-next-line
 import {RouterModule, Routes} from '@angular/router';
 
-const routes: Routes = [
-  {
-    path: 'shop',
-    loadChildren: () => import('./modules/shop/shop.module').then((m) => m.ShopModule),
-  }, 
-  { path: '', redirectTo: 'shop', pathMatch: 'full' },
-]
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-// eager loaded components 
-import { AppComponent } from './app.component';
-
-// modules 
 import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MaterialModule } from './modules/material/material.module';
-import { ShopModule } from './modules/shop/shop.module';
-
 
 @NgModule({
   declarations: [
-    AppComponent,
+    AppComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MaterialModule,
-    ShopModule
   ],
   providers: [],
   bootstrap: [AppComponent]
