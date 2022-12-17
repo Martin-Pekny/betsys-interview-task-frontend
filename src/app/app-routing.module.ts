4f8adb6c59fba8c7b6f7cfca5a926c4d22816129
Martin Pekný <84441995+Maperti@users.noreply.github.com>
2022-12-17 21:59:50 +0100

    feat: add base project module structure / simple nodejs server / angular material module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,17 @@ import {NgModule} from '@angular/core';
 // eslint-disable-next-line
 import {RouterModule, Routes} from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'shop',
+    loadChildren: () => import('./modules/shop/shop.module').then((m) => m.ShopModule),
+  }, 
+  { path: '', redirectTo: 'shop', pathMatch: 'full' },
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
